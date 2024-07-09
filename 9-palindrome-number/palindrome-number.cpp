a0ce83c55af56133d9fee4e5f08680cd5d9e47db
Ca5 <catinice@outlook.com>
2024-07-09 17:50:53 +0530

    Time: 14 ms (21.97%) | Memory: 8.2 MB (47.56%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        long long rev =0, last=0, copyx= x;
+        if (x<0){
+            return false;
+        }
+        while(x != 0){
+            last = x%10;
+            x=x/10;
+            rev = rev*10+last;
+
+        }
+        if(copyx == rev){
+            return true;
+        }
+        else{return false;}
+    }
+};
